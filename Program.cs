16b74c7ade0895628636e22b8027efc31c97988f
trixie-dev <mckinley55x@gmail.com>
2024-08-18 14:27:21 +0300

    new app behavior

--- Program.cs
+++ Program.cs
@@ -1,30 +1,148 @@
 ﻿using System;
+using System.Numerics;
 using Gtk;
 using Cairo;
 using Gdk;
+using gtk_test.utils;
+using Rectangle = Gdk.Rectangle;
 using Window = Gtk.Window;
 
 class MainClass
 {
-    public static void Main(string[] args)
+    static MainWindow mainWindow;
+    static List<NoteWindow> notes = new List<NoteWindow>();
+
+    static void Main(string[] args)
     {
         Application.Init();
 
-        MainWindow win = new MainWindow();
-        win.ShowAll();
+        mainWindow = new MainWindow();
+        mainWindow.Show();
+
+        // Додайте логіку для створення нових нотаток
+        CreateNewNote();
+        CreateNewNote();
 
         Application.Run();
     }
+
+    static void CreateNewNote()
+    {
+        NoteWindow note = new NoteWindow();
+        notes.Add(note);
+        note.ShowAll();
+    }
 }
 
-class MainWindow : Window
+class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        this.SkipTaskbarHint = true;
+        this.SkipPagerHint = true;
+        this.Decorated = false;
+        this.SetSizeRequest(1, 1);
+        this.Move(-9999, -9999);
+    }
+}
+
+class NoteWindow : Gtk.Window
+{
+    
+    private Label timeLabel;
+    private bool isDragging = false;
+    private int offsetX;
+    private int offsetY;
+    public NoteWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Vector2Int size = new Vector2Int(300, 100);
+        this.TypeHint = Gdk.WindowTypeHint.Utility;
+        this.Decorated = false;
+        AppPaintable = true;
+        //Visual = Screen.RgbaVisual;
+        this.SetSizeRequest(size.x, size.y);
+        WindowPosition = WindowPosition.None;
+        CenterTop(size);
+        
+        AddEvents((int)EventMask.ButtonPressMask | (int)EventMask.ButtonReleaseMask | (int)EventMask.PointerMotionMask);
+
+        ButtonPressEvent += OnButtonPressEvent;
+        ButtonReleaseEvent += OnButtonReleaseEvent;
+        MotionNotifyEvent += OnMotionNotifyEvent;
+        
+        // Створення мітки для відображення часу
+        timeLabel = new Label("00:00:00");
+        timeLabel.ModifyFont(Pango.FontDescription.FromString("Sans 48"));
+        timeLabel.ModifyFg(StateType.Normal, new Gdk.Color(255, 255, 255));
+
+        // Центрування мітки
+        Alignment alignment = new Alignment(0.5f, 0.5f, 0f, 0f);
+        alignment.Add(timeLabel);
+        Add(alignment);
+
+        // Оновлення часу кожну секунду
+        GLib.Timeout.Add(1000, UpdateTime);
+        
+        void CenterTop(Vector2Int size)
+        {
+            Screen screen = Screen.Default;
+            int monitor = screen.GetMonitorAtWindow(screen.RootWindow);
+            Rectangle monitorGeometry = screen.GetMonitorGeometry(monitor);
+
+            // Розрахунок позиції по центру зверху
+            int x = monitorGeometry.X + (monitorGeometry.Width - size.x) / 2;
+            int y = monitorGeometry.Y;
+
+            // Встановлення нової позиції вікна
+            Move(x, y);
+        }
+
+        void OnButtonPressEvent(object o, ButtonPressEventArgs args)
+        {
+            if (args.Event.Button == 1) // Ліва кнопка миші
+            {
+                isDragging = true;
+                offsetX = (int)args.Event.X;
+                offsetY = (int)args.Event.Y;
+            }
+        }
+
+        void OnButtonReleaseEvent(object o, ButtonReleaseEventArgs args)
+        {
+            if (args.Event.Button == 1) // Ліва кнопка миші
+            {
+                isDragging = false;
+            }
+        }
+
+        void OnMotionNotifyEvent(object o, MotionNotifyEventArgs args)
+        {
+            if (isDragging)
+            {
+                int x = (int)args.Event.XRoot - offsetX;
+                int y = (int)args.Event.YRoot - offsetY;
+                Move(x, y);
+            }
+        }
+
+        bool UpdateTime()
+        {
+            // Отримуємо поточний час у Києві
+            DateTime kyivTime = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.Now, "FLE Standard Time");
+            timeLabel.Text = kyivTime.ToString("HH:mm:ss");
+            return true; // Продовжувати оновлення
+        }
+    }
+}
+
+class ClockWindow : Window
 {
     private Label timeLabel;
     private bool isDragging = false;
     private int offsetX;
     private int offsetY;
 
-    public MainWindow() : base("Київський час")
+    public ClockWindow() : base("Київський час")
     {
         // Налаштування вікна
         SetDefaultSize(600, 60);
@@ -92,15 +210,5 @@ class MainWindow : Window
         DateTime kyivTime = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.Now, "FLE Standard Time");
         timeLabel.Text = kyivTime.ToString("HH:mm:ss");
         return true; // Продовжувати оновлення
-    }
-
-    private void OnDraw(object o, DrawnArgs args)
-    {
-        using (Context cr = args.Cr)
-        {
-            cr.SetSourceRGBA(0.1, 0.1, 0.1, 0.6); // Темно-сірий колір з прозорістю 60%
-            cr.Operator = Operator.Source;
-            cr.Paint();
-        }
     }
 }
